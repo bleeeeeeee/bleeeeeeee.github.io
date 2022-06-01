f6e1acc813174be706cf3ee265897fce1f68a4e1
Miłosz Brzechczyn <miloszbrzechczyn@gmail.com>
2022-06-01 14:08:57 +0200

    Class simplified

--- src/gui/Button.ts
+++ src/gui/Button.ts
@@ -1,7 +1,10 @@
+// Beware of the horrifying line below!
+// TODO: either write typescript definitions file for troika-three-text or find a better way to implement text rendering.
+// @ts-nocheck
+
 import * as THREE from "three";
 
-import * as TROIKA from "troika-three-text";
-// import { Text } from "troika-three-text";
+import { Text } from "troika-three-text";
 
 export interface ButtonParameters {
 
@@ -17,7 +20,7 @@ export interface ButtonParameters {
 export class Button extends THREE.Group {
 
     private readonly shape: THREE.Mesh;
-    private readonly text: TROIKA.Text;
+    private readonly text: Text;
 
     public constructor(params: ButtonParameters) {
 
@@ -41,7 +44,7 @@ export class Button extends THREE.Group {
         this.text.color     = params.fontColor || "0x000000";
         // this.text.textAlign = "center";
         this.text.maxWidth  = params.size.x;
-        this.text.anchorX   = params.size.x / 2 - (text.text.length == 4 ? 4.8 : 3.2);
+        this.text.anchorX   = params.size.x / 2 - (this.text.text.length == 4 ? 4.8 : 3.2);
         this.text.anchorY   = -params.position.y - params.size.y / 2 + 0.18;
 
         this.add(this.text);
